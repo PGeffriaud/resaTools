cd204e0f19a8d5d3e6d7ba387e149b2465c05c89
JeanBaptisteR <robert.jeanbaptiste@laposte.net>
2015-10-21 10:44:09 +0200

    [JBR] Modification on login

--- src/main/java/org/emn/resa/servlets/TemplateServlet.java
+++ src/main/java/org/emn/resa/servlets/TemplateServlet.java
@@ -2,20 +2,15 @@ package org.emn.resa.servlets;
 
 import java.io.IOException;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.emn.resa.entities.User;
 import org.emn.resa.managers.RessourceManager;
+import org.emn.resa.managers.ConnexionManager;
 
 /**
  * Servlet implementation class TemplateServlet
@@ -23,103 +18,90 @@ import org.emn.resa.managers.RessourceManager;
 @WebServlet("/action/*")
 public class TemplateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TemplateServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String fullPath = request.getPathInfo().substring(1);
-		String[] paths = fullPath.split("/");
-		switch(paths[0]){
-			case "ressources": 
-				if(paths.length > 1){
-					switch (paths[1]) {
-						case "addtype": 
-							RessourceManager.addType(request.getParameter("typeName"));
-							break;
-						case "deltype":
-							RessourceManager.deleteType(Integer.parseInt(request.getParameter("id")));
-							break;
-						case "updatetype": break;
-						case "address": break;
-						case "delress": break;
-						case "updateress": break;
-						default: break;
-					}
-				}
-				request.getSession().setAttribute("listType", RessourceManager.getTypeList());
-				break;
-			case "reservations": break;
-			case "user": break;
-			case "login": break;
-			case "logout": break;
-			default: break;
-		}
-		
-		String path = request.getPathInfo();
-		if(path.substring(1).equals("login")){
-			RequestDispatcher rd = getServletContext().getRequestDispatcher("/jsp/login.jsp");
-			rd.forward(request, response);
-		}	
-		else{
-			request.setAttribute("page", paths[0]);
-			RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
-			rd.forward(request, response);
-		}	
+	public TemplateServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String fullPath = request.getPathInfo().substring(1);
+		String[] paths = fullPath.split("/");
 
-		HttpSession session = request.getSession(true);
-		
-		if(request.getParameter("buttonDeconnexion") != null){
-			request.getSession().invalidate();
-			RequestDispatcher rd = getServletContext().getRequestDispatcher("/jsp/login.jsp");
-			rd.forward(request, response);
-		}
-		else if(request.getParameter("buttonConnexion") != null){
-			String login = request.getParameter("login");
-			String pass = request.getParameter("pass");
-			System.out.println(login);
-			EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("resaTools");
-			EntityManager entityManager = entityManagerFactory.createEntityManager();
-			entityManager.getTransaction().begin();
-			Query q = entityManager.createQuery("SELECT u FROM User u WHERE u.login = :login AND u.password = :pass");
-			q.setParameter("login", login);
-			q.setParameter("pass", pass);
-			
-			try{
-				User user = (User) q.getSingleResult();
-				if (login.equalsIgnoreCase(user.getLogin()) && pass.equals(user.getPassword())) {
-					System.out.println("Success");
-			        session.setAttribute("currentSessionUser",user); 
-			        request.setAttribute("page", "accueil");
-			        RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
-			        rd.forward(request, response);
+		switch (paths[0]) {
+		case "ressources":
+			if (paths.length > 1) {
+				switch (paths[1]) {
+				case "addtype":
+					RessourceManager.addType(request.getParameter("typeName"));
+					break;
+				case "deltype":
+					RessourceManager.deleteType(Integer.parseInt(request.getParameter("id")));
+					break;
+				case "updatetype":
+					break;
+				case "address":
+					break;
+				case "delress":
+					break;
+				case "updateress":
+					break;
+				default:
+					break;
 				}
 			}
-			catch (Exception e){
-				System.out.println("Exception : " + e.getMessage());
-				session.setAttribute("connectedTried",true);
+			request.getSession().setAttribute("listType", RessourceManager.getTypeList());
+			break;
+		case "reservations":
+			break;
+		case "user":
+			break;
+		case "login":
+			String login = request.getParameter("login");
+			String pass = request.getParameter("pass");
+			if (ConnexionManager.connect(login, pass) != null) {
+				request.getSession().setAttribute("currentSessionUser", ConnexionManager.connect(login, pass));
+				request.setAttribute("page", "accueil");
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
+				rd.forward(request, response);
+			} else {
+				if (login != null) {
+					request.getSession().setAttribute("connectedTried", true);
+				}
 				RequestDispatcher rd = getServletContext().getRequestDispatcher("/jsp/login.jsp");
 				rd.forward(request, response);
 			}
-			finally{
-				entityManagerFactory.close();
-				entityManager.close();
-			}
+			break;
+		case "logout":
+			request.getSession().invalidate();
+			response.sendRedirect(request.getContextPath() + "/action/login");
+			break;
+		default:
+			break;
 		}
+
+		if (!paths[0].equals("login") && !paths[0].equals("logout")) {
+			request.setAttribute("page", paths[0]);
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
+			rd.forward(request, response);
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
 	}
 }
